c12b2f4bc308b9b90395e2d80d12c35a5582b48e
Sarup Chakraborty <sarupmatiari@gmail.com>
2022-12-31 11:33:27 +0530

    Primitives number checking using Java

---
+++ Primitives.java
@@ -0,0 +1,12 @@
+package com.sarup;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 46675654.4654;
+        long largeInteger = 545456846575657697L;
+        boolean check = false;
+    }
+}
